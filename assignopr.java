a3dd4733a6c6fe4f13a7b2175dedf9bb8155ab78
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-06-17 14:02:41 +0530

    Create assignopr.java

---
+++ assignopr.java
@@ -0,0 +1,15 @@
+class Assignment
+{
+public static void main(String[] args)
+{
+ int a=25;
+int b=12;
+System.out.println("Addition Assignment of "+a+" and "+b+" is: "+(a+=b));
+System.out.println("Subtraction Assignment of "+a+" and "+b+" is: "+(a-=b));
+System.out.println("Division Assignment of "+a+" and "+b+" is: "+(a%=b));
+System.out.println("Multiplication Assignment of "+a+" and "+b+" is: "+(a*=b));
+System.out.println("Modulo Division Assignment of "+a+" and "+b+" is: "+(a%=b));
+}
+}
+
+  
