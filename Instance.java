79ac97f6226de912087210cf7fc66f87a337d588
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-06-17 21:26:17 +0530

    Create Instance.java

---
+++ Instance.java
@@ -0,0 +1,13 @@
+class Instance{
+    int num;
+    void display() 
+{
+  System.out.println("Default int: " + num);
+   }
+
+    public static void main(String[] args) 
+{
+       Instance obj= new Instance();
+        obj.display();
+    }
+}
