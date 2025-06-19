94f16f76b239611ca06d0149644f6d6da2e4b794
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-06-19 12:08:57 +0530

    Create Var.java

---
+++ Var.java
@@ -0,0 +1,36 @@
+/*Instance*/
+/*
+class Var{
+    int num;
+    void display() 
+{
+  System.out.println("Default int: " + num);
+   }
+
+    public static void main(String[] args) 
+{
+       Instance obj= new Instance();
+        obj.display();
+    }
+}*/
+/*Static*/
+/*class Var{
+   static int num;
+    void display() 
+{
+  System.out.println("Default int: " + num);
+   }
+
+    public static void main(String[] args) 
+{
+        Static obj = new Static();
+        obj.display();
+    }
+}*/
+/*Local*/
+class Var{
+    public static void main(String[] args) {
+        int number=99; 
+        System.out.println("Local int: " + number);
+    }
+}
