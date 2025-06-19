510bfc818cbcfc436c6ed232cf1b107c57d2b232
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-06-19 11:41:30 +0530

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,60 @@
+/* With return type and with parameter*/
+/*
+class Method
+{
+public int add(int a,int b)
+{
+	return(a+b);
+}
+public static void main (String [] args)
+{
+Method m=new Method();
+int c;
+c=m.add(10,11);
+System.out.print(c);
+}
+}*/
+/*Without return type and without parameters*/
+/*
+class Method 
+{
+public void sub()
+{
+System.out.print(46-15);
+}
+public static void main(String[] args)
+{
+Method m=new Method();
+m.sub();
+}
+}*/
+/*With parameter and no return type*/
+/*
+class Method
+{
+public void mul(int a,int b)
+{
+System.out.print(a*b);
+}
+public static void main(String [] args)
+{
+Method m=new Method();
+m.mul(45,2);
+}
+}*/
+/*With return type and with parameters*/
+class Method 
+{
+public int Sub()
+{
+  int a=134-34;
+return(a);
+}
+public static void main(String [] args)
+{
+Method m=new Method();
+int b=m.Sub();
+System.out.print(b);
+}
+}
+	
