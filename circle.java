9815ef80a06e12c559c652614dacc8a582750a39
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-04-17 21:37:40 +0530

    Add circle area calculation program in circle.java
    Using the scanner object to the input from the user.

---
+++ circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class circle {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the circle radius: ");
+        double r=sc.nextDouble();
+        double area=3.14*r*r;
+        System.out.println("The area of the circle with radius "+r+" is: "+area);
+    }
+    
+}
