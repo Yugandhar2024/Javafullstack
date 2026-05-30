87cc065f0bdba2d739ec33e47ea72e5bb1e74162
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-05-30 14:50:29 +0530

    Basic print statements using sop and sopln.

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+class Demo1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Sasank");
+		System.out.print("hi");
+		System.out.print("Good morning");
+		System.out.println("What are you doing ?");
+		System.out.print("Iam Fine");
+		System.out.print("ok");
+		System.out.println("Bye");
+		System.out.print(true);
+		System.out.println(false);
+		System.out.print(10);
+	}
+}
