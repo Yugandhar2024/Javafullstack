ee51e5c32cb46c3ec3ac8a373ced35547c253292
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-06-08 17:18:25 +0530

    Bitwise1 operations performed 8-6

---
+++ Bitwise1.java
@@ -0,0 +1,18 @@
+class Bitwise1{
+
+public static void main(String[] args)
+	{
+
+		int a=15,b=17;
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(~b);
+		System.out.println(18<<3);
+		System.out.println(10>>2);
+		System.out.println(10>>>2);
+
+
+	}
+
+}
