689cc260e76c143ab2c6a0e859b4f02dc6b04483
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-06-13 00:25:51 +0530

    Display the menu using Switch case

---
+++ Menu.java
@@ -0,0 +1,50 @@
+class Menu{
+public static void main(String[] args)
+	{
+	int day=5;
+	switch (day) {
+
+		case 1: 
+		
+			System.out.println("Idly");
+			break;
+		
+		case 2: 
+		
+			System.out.println("Dosa");
+			break;
+		
+		case 3: 
+		
+			System.out.println("Upma");
+			break;
+		
+		case 4: 
+		
+			System.out.println("Bonda");
+			break;
+		
+		case 5: 
+		
+			System.out.println("Pulihora");
+			break;
+		
+		case 6: 
+		
+			System.out.println("Baggara Rice");
+			break;
+		
+		case 7: 
+		
+			System.out.println("Egg Dosa");
+			break;
+		
+		default:
+		
+			System.out.println("Invalid");
+		
+
+ 
+	}
+}
+}
