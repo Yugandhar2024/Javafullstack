b3cd4bc8e81745827368699cea8995d52381a88c
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-06-13 00:27:05 +0530

    Add Rem class to check divisibility by 5 and 11

---
+++ Rem.java
@@ -0,0 +1,12 @@
+class Rem
+{
+public static void main(String[] args)
+{
+	int n=50;
+	if((n%5==0) && (n%11==0))
+		System.out.println("Number will divisible by 5 and 11 is :" +n);
+	else
+		System.out.println("Number will not divisible by 5 and 11 is :" +n);
+}
+}
+		
