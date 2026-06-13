be095fef596372aad4e4a292cb54d29c2fab6a0e
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-06-13 14:10:47 +0530

    Add Vow class to check vowels and consonants
    Implement a program to check if a character is a vowel or consonant.

---
+++ Vow.java
@@ -0,0 +1,12 @@
+class Vow{
+public static void main(String[] args){
+
+		//program to check wheater given character is vowel or consonant
+	char v='a';
+	if(v=='a' || v=='e' || v=='i' || v=='o' || v=='u' || v=='A' || v=='E' || v=='I' || v=='O' || v=='U')
+		System.out.println("Given Character is Vowel.");
+	else
+		System.out.println("Given Character is Consonant.");
+
+}
+}
