2bb90b42a309d3c9af03b368fe01d38ce2ed653f
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-06-26 23:44:48 +0530

    Implement add,sub,mult,div,modulo divi method in Demo4 class

---
+++ Demo4.java
@@ -0,0 +1,64 @@
+/* class Demo4 //Addition Method.
+{
+	public static int add(int a,int b)
+{
+	int c=a+b;
+	return c;
+}
+	public static void main(String[] args)
+{
+	System.out.println("Addition of two numbers: "+add(5,77));
+}
+}*/
+/*class Demo4 //Subtraction Method.
+
+{
+	public static int sub(int a,int b)
+{
+	int c=a-b;
+	return c;
+}
+	public static void main(String[] args)
+{
+	System.out.println("Subtraction of two numbers: "+sub(95,77));
+}
+}
+*/
+/*class Demo4 //Multiplication Method.
+{
+	public static int mult(int a,int b)
+{
+	int c=a*b;
+	return c;
+}
+	public static void main(String[] args)
+{
+	System.out.println("Multiplication of two numbers: "+mult(11,77));
+}
+}
+*/
+/*class Demo4 //Division Method.
+{
+	public static int div(int a,int b)
+{
+	int c=a/b;
+	return c;
+}
+	public static void main(String[] args)
+{
+	System.out.println("Division of two numbers: "+div(22,2));
+}
+}
+*/
+class Demo4 //Modulo Division Method
+{
+	public static int mod(int a,int b)
+{
+	int c=a%b;
+	return c;
+}
+	public static void main(String[] args)
+{
+	System.out.println("Modulo Division of two numbers: "+mod(220,2));
+}
+}
