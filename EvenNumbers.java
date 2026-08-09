e18cb7887a0520defe65f0053e5f9172727751c5
Kunasani Yugandhar <kunasaniyugandhar2004@gmail.com>
2026-08-09 22:51:20 +0530

    Add EvenNumbers class to find even values

---
+++ EvenNumbers.java
@@ -0,0 +1,22 @@
+package day_1;
+import java.util.Scanner;
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter how many values you want to store: ");
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		int[] arr=new int[size];
+		for(int i=0 ; i<size ; i++) {
+			System.out.println("Enter the "+i+" index value: ");
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("---------------------------------------");
+		System.out.println("Given array Even values are :");
+		for(int i=0; i<size ; i++) {
+			if(arr[i]%2==0) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+}
